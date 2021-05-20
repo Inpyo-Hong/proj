63f164484790f70cfbe263f1e689d0f2a80cca93
yejinnnn <najin2445@naver.com>
2021-05-20 18:40:44 +0900

    today_exercise_add

--- app/src/main/java/com/cookandroid/newproj/Today_Exercise/today_exercise.java
+++ app/src/main/java/com/cookandroid/newproj/Today_Exercise/today_exercise.java
@@ -2,30 +2,56 @@ package com.cookandroid.newproj.Today_Exercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.cookandroid.newproj.R;
-import com.cookandroid.newproj.Today_Exercise.Camera.CameraActivity;
+
+import java.util.ArrayList;
 
 public class today_exercise extends AppCompatActivity {
 
-    private Button btn_today_squat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_today_exercise);
 
-        btn_today_squat = findViewById(R.id.btn_today_squat);
+        final ArrayList<String> items = new ArrayList<String>() ;
+        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, items) ;
 
-        btn_today_squat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(today_exercise.this, CameraActivity.class);
-                startActivity(intent);
+        final ListView listview = (ListView) findViewById(R.id.listview1) ;
+        listview.setAdapter(adapter) ;
+
+        Button addButton = (Button)findViewById(R.id.add) ;
+        addButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                int count;
+                count = adapter.getCount();
+
+                items.add("LIST" + Integer.toString(count + 1));
+                adapter.notifyDataSetChanged();
             }
-        });
+        }) ;
+
+        Button deleteButton = (Button)findViewById(R.id.delete) ;
+        deleteButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                int count, checked ;
+                count = adapter.getCount() ;
+
+                if (count > 0) {
+                    checked = listview.getCheckedItemPosition();
+
+                    if (checked > -1 && checked < count) {
+                        items.remove(checked) ;
+                        listview.clearChoices();
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            }
+        }) ;
     }
 }
